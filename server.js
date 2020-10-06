2e9f1394635549ff06b73690eb44bdf11ca2fb51
Natalie Garcia <gnatalie.booking@gmail.com>
2020-10-06 11:32:24 -0400

    updating to connect to mongodb to heroku

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
+{ useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+ });
 
 
 //Routes
